7b54ca43875301209b0b98d1efc7e191b444b6e6
Msk6 <Maha.s.kh@hotmail.com>
2020-08-17 14:19:46 +0300

    solution

--- age_calculator.py
+++ age_calculator.py
@@ -1,14 +1,32 @@
-from datetime import datetime
+import datetime
+
 
 def check_birthdate(year, month, day):
 	# write code here
+	x = datetime.datetime.now()
+	y = datetime.datetime(year, month, day)
+	return y < x
 
 def calculate_age(year, month, day):
     # write code here
+	x = datetime.datetime.now()
+	y = datetime.datetime(year, month, day) 
+	print ("You are %s years old." %((x-y).days/365))
+	return
 
 def main():
 	# write main code here
+	year = int(input("Enter year of birth: "))
+	month = int(input("Enter month of birth: "))
+	day = int(input("Enter day of birth: "))
 
+	if check_birthdate(year, month, day):
+		calculate_age(year, month, day)
+		
+	else:
+		print ("please enter valid date")
+
+	
 
 if __name__ == '__main__':
 	main()
